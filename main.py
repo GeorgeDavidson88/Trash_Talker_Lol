1cf8b2bdeb4227abeca8d476019d48f8a820719c
George Davidson <gd240673@gmail.com>
2022-12-31 17:19:57 -0500

    No Repeat
    You will now never repeat the same phrase twice

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ import pynput
 exit_threads = threading.Event()
 
 phrases = []
+endings = []
+last_phrase = ""
+last_ending = ""
 
 with open("phrases.txt") as phrases_file:
     for line in phrases_file:
         phrases.append(line.strip())
+
+with open("endings.txt") as endings_file:
+    for line in endings_file:
+        endings.append(line.strip())
 
 
 def say(string):
@@ -26,7 +33,7 @@ def on_press(key):
         return False
 
     if str(key) == "Key.f1":
-        say("Trash kid :(")
+        say("Trash Kid")
 
     elif str(key) == "Key.f2":
         say("Easy :)")
@@ -35,10 +42,49 @@ def on_press(key):
         say("DEAD LOL")
 
     elif str(key) == "Key.f4":
-        say("sucks to suck :)")
+        say("BAD XD")
 
     elif str(key) == "Key.f8":
-        say(phrases[random.randint(0, len(phrases) - 1)])
+        global last_phrase
+        global last_ending
+
+        phrase_index = random.randint(0, len(phrases) - 1)
+        ending_index = random.randint(0, len(endings) - 1)
+
+        phrase = phrases[phrase_index]
+        ending = endings[ending_index]
+
+        if last_phrase.lower() == phrase.lower():
+            if len(phrases) != 1:
+                if phrase_index == 0:
+                    phrase = phrases[phrase_index + 1]
+                elif phrase_index == len(phrases) - 1:
+                    phrase = phrases[phrase_index - 1]
+                else:
+                    phrase = phrases[phrase_index + 1]
+
+        if last_ending.lower() == ending.lower():
+            if len(endings) != 1:
+                if ending_index == 0:
+                    ending = endings[ending_index + 1]
+                elif ending_index == len(endings) - 1:
+                    ending = endings[ending_index - 1]
+                else:
+                    ending = endings[ending_index + 1]
+
+        random_number = random.randint(0, 2)
+
+        if random_number == 0:
+            phrase = phrase.lower()
+        elif random_number == 1:
+            phrase = phrase.upper()
+        else:
+            phrase = phrase.title()
+
+        say(f"{phrase} {ending}")
+
+        last_phrase = phrase
+        last_ending = ending
 
 
 def keyboard_listener():
@@ -47,20 +93,21 @@ def keyboard_listener():
 
 
 def main():
-    print("Program Started.")
+    print("Program Started...")
 
     keyboard_listener_thred = threading.Thread(target=keyboard_listener)
     keyboard_listener_thred.start()
 
     while True:
-        command = input("Type 'quit' to quit>")
+        command = input("Type 'quit' to quit or close the terminal -> ")
 
         if command == "quit":
             break
 
     exit_threads.set()
     pynput.keyboard.Controller().tap(pynput.keyboard.Key.esc)
+    print("Program Stoped.")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" and len(phrases) != 0:
     main()
