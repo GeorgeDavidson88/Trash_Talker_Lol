7ef26edf25798fd9ff605bab29ebd6f6a45dc250
George Davidson <gd240673@gmail.com>
2023-01-03 22:23:42 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Roblox Trash Talker
 
-:)
+A roblox trash talker because :)
+
+## Instructions
+
+### Installation
+
+Make sure you have pynput installed.
+
+```python
+pip install pynput # install pynput with pip
+```
+
+### How To Use
+
+Tap F1, F2, F3, or F4 to say a specific phrase. You can change the phrase in main.py.
+
+Tap F8 to say a random phrase from phrases.txt.
+
+### Things To Note
+
+The endings.txt file is to add more variety to the phrases.
--- main.py
+++ main.py
@@ -7,8 +7,14 @@ import pynput
 
 class Main:
     def __init__(self):
+        self.F1_Phrase = "Trash Kid"
+        self.F2_Phrase = "Easy :)"
+        self.F3_Phrase = "DEAD LOL"
+        self.F4_Phrase = "BAD XD"
+
         self.phrases = []
         self.endings = []
+
         self.last_phrase = ""
         self.last_ending = ""
 
@@ -30,58 +36,68 @@ class Main:
 
     def on_press(self, key):
         if str(key) == "Key.f1":
-            self.say("Trash Kid")
+            self.say(self.F1_Phrase)
 
         elif str(key) == "Key.f2":
-            self.say("Easy :)")
+            self.say(self.F2_Phrase)
 
         elif str(key) == "Key.f3":
-            self.say("DEAD LOL")
+            self.say(self.F3_Phrase)
 
         elif str(key) == "Key.f4":
-            self.say("BAD XD")
+            self.say(self.F4_Phrase)
 
         elif str(key) == "Key.f8":
-            # Selects a random index from the list of phrases.
-            self.phrase_index = random.randint(0, len(self.phrases) - 1)
-            # Selects a random index from the list of endings.
-            self.ending_index = random.randint(0, len(self.endings) - 1)
+            if len(self.phrases) != 0:
+                # Selects a random index from the list of phrases.
+                self.phrase_index = random.randint(0, len(self.phrases) - 1)
 
-            # Set the current phrase to the random phrase index.
-            phrase = self.phrases[self.phrase_index]
-            # Set the current ending to the random ending index.
-            ending = self.endings[self.ending_index]
+                # Set the current phrase to the random phrase index.
+                phrase = self.phrases[self.phrase_index]
 
-            if self.last_phrase.lower() == phrase.lower():
-                # We will not throw an error if there is only one phrase in phrases.txt.
-                if len(self.phrases) != 1:
-                    if self.phrase_index == 0:  # We can't pick an index that is lower than 0.
-                        phrase = self.phrases[self.phrase_index + 1]
-                    # We can't choose an index greater than the length of phrases.txt.
-                    elif self.phrase_index == len(self.phrases) - 1:
-                        phrase = self.phrases[self.phrase_index - 1]
-                    else:
-                        # We will pick the phrase that is one index above the current phrase.
-                        phrase = self.phrases[self.phrase_index + 1]
+                if self.last_phrase.lower() == phrase.lower():
+                    # We will not throw an error if there is only one phrase in phrases.txt.
+                    if len(self.phrases) > 1:
+                        if self.phrase_index == 0:  # We can't pick an index that is lower than 0.
+                            phrase = self.phrases[self.phrase_index + 1]
+                        # We can't choose an index greater than the length of phrases.txt.
+                        elif self.phrase_index == len(self.phrases) - 1:
+                            phrase = self.phrases[self.phrase_index - 1]
+                        else:
+                            # We will pick the phrase that is one index above the current phrase.
+                            phrase = self.phrases[self.phrase_index + 1]
 
-            if self.last_ending.lower() == ending.lower():  # The same logic as above.
-                if len(self.endings) != 1:
-                    if self.ending_index == 0:
-                        ending = self.endings[self.ending_index + 1]
-                    elif self.ending_index == len(self.endings) - 1:
-                        ending = self.endings[self.ending_index - 1]
-                    else:
-                        ending = self.endings[self.ending_index + 1]
+                # To add more variety to the phrases.
+                random_number = random.randint(0, 2)
 
-            # To add more variety to the phrases.
-            random_number = random.randint(0, 2)
+                if random_number == 0:
+                    phrase = phrase.lower()
+                elif random_number == 1:
+                    phrase = phrase.upper()
+                else:
+                    phrase = phrase.title()
 
-            if random_number == 0:
-                phrase = phrase.lower()
-            elif random_number == 1:
-                phrase = phrase.upper()
             else:
-                phrase = phrase.title()
+                phrase = ""
+
+            if len(self.endings) != 0:
+                # Selects a random index from the list of endings.
+                self.ending_index = random.randint(0, len(self.endings) - 1)
+
+                # Set the current ending to the random ending index.
+                ending = self.endings[self.ending_index]
+
+                if self.last_ending.lower() == ending.lower():  # The same logic as above.
+                    if len(self.endings) > 1:
+                        if self.ending_index == 0:
+                            ending = self.endings[self.ending_index + 1]
+                        elif self.ending_index == len(self.endings) - 1:
+                            ending = self.endings[self.ending_index - 1]
+                        else:
+                            ending = self.endings[self.ending_index + 1]
+
+            else:
+                ending = ""
 
             self.say(f"{phrase} {ending}")
 
@@ -107,14 +123,13 @@ def main():
 
     main = Main()
 
-    if len(main.phrases) != 0:
-        # Create and start the keyboard_listener_thread.
-        keyboard_listener_thred = threading.Thread(
-            target=keyboard_listener, args=(main,))
-        keyboard_listener_thred.daemon = True
-        keyboard_listener_thred.start()
+    # Create and start the keyboard_listener_thread.
+    keyboard_listener_thred = threading.Thread(
+        target=keyboard_listener, args=(main,))
+    keyboard_listener_thred.daemon = True
+    keyboard_listener_thred.start()
 
-        main.main_loop()
+    main.main_loop()
 
 
 if __name__ == "__main__":
